d0b54e4ed2710d0daf993a06a950010017b8d1a1
shinilcu <shinil.unnikuttan@gmail.com>
2025-07-25 08:03:51 -0400

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://ad9ec2f0ba0e44df7ba22e6f66254e52-2108036352.us-east-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://af5b2306011504bb59412a2d57d1e1fb-927538733.us-east-1.elb.amazonaws.com',
 };
